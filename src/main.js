67a3795407349050f3a12f4833717efdf51b75be
hobbit lau <hiufailau.41@gmail.com>
2018-11-15 18:39:27 +0100

    some json data

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import firebase from "firebase/app";
 Vue.config.productionTip = false
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC--KN7VpPm6JRomF4a9fYdKNwZ3e6gCUU",
+  authDomain: "chatdatabase-d32ec.firebaseapp.com",
+  databaseURL: "https://chatdatabase-d32ec.firebaseio.com",
+  projectId: "chatdatabase-d32ec",
+  storageBucket: "",
+  messagingSenderId: "419301994310"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
